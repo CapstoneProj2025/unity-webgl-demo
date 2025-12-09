f04edf6920e3246905bde6830185e962047685c1
CapstoneProj2025 <matthewfitness01@gmail.com>
2025-12-09 19:54:02 +1300

    Initial commit - Unity WebGL hosting project

---
+++ script.js
@@ -0,0 +1,41 @@
+const buildUrl = "Build";
+const config = {
+  dataUrl: buildUrl + "/YourGameName.data",
+  frameworkUrl: buildUrl + "/YourGameName.framework.js",
+  codeUrl: buildUrl + "/YourGameName.wasm",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "My Company",
+  productName: "My Game",
+  productVersion: "1.0",
+};
+
+const canvas = document.getElementById("unity-canvas");
+const loadingBar = document.getElementById("unity-loading-bar");
+const progressBarFull = document.getElementById("unity-progress-bar-full");
+const warningBanner = document.getElementById("unity-warning");
+
+function unityShowBanner(msg, type) {
+  warningBanner.style.display = "block";
+  warningBanner.textContent = msg;
+  if (type === "error") {
+    warningBanner.style.color = "#ff4c4c";
+  } else {
+    warningBanner.style.color = "#ffb347";
+  }
+
+  if (type !== "error") {
+    setTimeout(() => {
+      warningBanner.style.display = "none";
+    }, 5000);
+  }
+}
+
+createUnityInstance(canvas, config, (progress) => {
+  loadingBar.style.display = "block";
+  progressBarFull.style.width = (100 * progress) + "%";
+}).then((unityInstance) => {
+  loadingBar.style.display = "none";
+  window.unityInstance = unityInstance;
+}).catch((message) => {
+  unityShowBanner("Failed to load Unity WebGL build: " + message, "error");
+});
